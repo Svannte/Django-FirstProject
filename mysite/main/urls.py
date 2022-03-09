01d0ccede1b2a3b468c2845480ac87a884177906
Mateusz Łuczyński <kptw1@wp.pl>
2022-03-09 16:30:42 +0100

    Initial commit

---
+++ mysite/main/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("<int:idd>", views.index, name="index"),
+    path("", views.home, name="home"),
+    path("create/", views.create, name="create"),
+    path('home/', views.home, name="home"),
+    path("view/", views.view, name="view")
+]
